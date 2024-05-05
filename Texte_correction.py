ee456ad7824f093f2a30893a0ede975d4d47eb58
MateoDib <161327318+MateoDib@users.noreply.github.com>
2024-05-06 00:34:47 +0200

    Add files via upload

---
+++ Texte_correction.py
@@ -0,0 +1,58 @@
+import os
+import replicate
+import pyperclip
+import re  # Ajout de la bibliothèque regex pour extraire le texte entre guillemets
+
+
+# Définir la variable d'environnement
+os.environ['REPLICATE_API_TOKEN'] = 'r8_MLzTVH5MhKXLo6HibElHzfj27o1LOwH29uv31'
+
+# Assurez-vous que la variable d'environnement est définie
+REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
+if REPLICATE_API_TOKEN is None:
+    raise EnvironmentError("REPLICATE_API_TOKEN is not set in the environment")
+
+# Configuration initiale de Replicate
+replicate.api_token = REPLICATE_API_TOKEN
+
+
+def get_clipboard():
+    """Récupère le texte du presse-papiers."""
+    return pyperclip.paste()
+
+def correct_text_with_replicate(text):
+    """Envoie le texte à l'API Replicate pour correction et extrait uniquement le texte corrigé."""
+    input = {
+        "prompt": f"Corrige les fautes d'orthographe, de grammaire, de syntaxe, de conjugaison, etc., dans le texte suivant et retourne uniquement le texte corrigé dans la langue originale, placé entre guillemets : \"{text}\"",
+        "prompt_template": "system\n\nTu es un Assistant très efficace en traduction et tu dois suivre les instructions suivantes :\n\n{prompt}\n\n"
+    }
+    try:
+        output = replicate.run("meta/meta-llama-3-70b-instruct", input=input)
+        response_text = "".join(output)  # Joindre les parties de la réponse
+        # Extraire le texte corrigé entre guillemets
+        corrected_text = re.findall(r'\"(.*?)\"', response_text)
+        if corrected_text:
+            return corrected_text[0]
+        else:
+            return "Aucun texte corrigé trouvé."
+    except Exception as e:
+        print(f"Erreur lors de l'envoi de la demande à Replicate: {e}")
+        return None
+
+def main():
+    text_from_clipboard = get_clipboard()
+    print(f"Texte récupéré du presse-papiers : {text_from_clipboard}")
+    
+    corrected_text = correct_text_with_replicate(text_from_clipboard)
+    if corrected_text:
+        print("Texte corrigé de Replicate:")
+        print(corrected_text)
+        pyperclip.copy(corrected_text)
+        print("Réponse corrigée copiée de nouveau dans le presse-papiers.")
+    else:
+        print("Aucune réponse reçue ou erreur lors de la récupération de la réponse.")
+
+if __name__ == "__main__":
+    main()
+    
+
